6cd86d6e648a7fae4472c127560af91d5056da70
lamamurad20 <lamaraed2k23@gmail.com>
2025-02-25 22:37:44 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/about', function(){
+    $name='Lama';
+    $departments = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    //return view('about', ['name' => $name]);
+    //return view('about')->with('name',$name);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function(){
+    $name = $_POST['name'];
+    $departments = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    return view('about', compact('name'));
+});
